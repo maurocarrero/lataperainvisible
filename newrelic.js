0bdaefbda5a033e1b1208e63bad267af968f22a0
Mauro Carrero <mauro.carrero@globant.com>
2014-12-06 14:44:47 -0200

    [New Relic]
    Adds New Relic addon - fixing

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['lataperainvisible'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '8d617d12c93439496c5aa395d9b1c3413b335fc5',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'info'
+  }
+};
