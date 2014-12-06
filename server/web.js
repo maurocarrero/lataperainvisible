7a9c8cc3af7072a54a77934cf405314b14f4819e
Mauro Carrero <mauro.carrero@globant.com>
2014-12-06 16:11:16 -0200

    [NEW RELIC]
    Taqks care of this addon... again

--- server/web.js
+++ server/web.js
@@ -2,8 +2,6 @@ var express = require('express');
 var app = express();
 var newrelic = require('newrelic');
 
-console.log(newrelic);
-
 // EXPRESS SERVER
 app.use(express.logger());
 app.use(express.static(__dirname + '/app'));
