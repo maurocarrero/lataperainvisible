c0587b1ae97d8be217d9206d119b2638ca84e15a
Mauro <maurocarrero@lataperainvisible.com.uy>
2014-12-10 23:32:59 -0200

    [Imagemin error]
    Removes compass:dist from concurrent:server task

--- Gruntfile.js
+++ Gruntfile.js
@@ -302,7 +302,7 @@ module.exports = function (grunt) {
     // Run some tasks in parallel to speed up the build process
     concurrent: {
       server: [
-        'compass:server',
+        //'compass:server',
         'copy:styles'
       ],
       test: [
